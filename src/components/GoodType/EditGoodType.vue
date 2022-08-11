b4a9c743614fdc64ca37e09f9c48db4aca96c3d8
GouYubo <1366407060@qq.com>
2022-08-11 15:52:51 +0800

    fix #62070183 商品类别管理

---
+++ src/components/GoodType/EditGoodType.vue
@@ -0,0 +1,36 @@
+<template>
+  <el-form :inline="true"  class="demo-form-inline">
+    <el-form-item label="名称">
+      <el-input v-model="GName" placeholder="请输入名称"></el-input>
+    </el-form-item>
+    <el-form-item>
+      <el-button type="primary" @click="onSubmit">更改 </el-button>
+    </el-form-item>
+  </el-form>
+</template>
+<script>
+import {editGoodTypeApi} from "../../utils/api";
+
+export default {
+  name:'EditGoodType',
+  data() {
+    return {
+      GName: '',
+      GId:this.$route.query.goodId
+    }
+  },
+  methods: {
+    onSubmit() {
+      console.log('submit!');
+      editGoodTypeApi({'name':this.GName,'id':this.GId}).then(res=>{
+        this.$message.success(
+            {
+              message:'修改成功',
+              type:"success"
+        })
+        this.$router.push('/home/goodsType')
+      })
+    }
+  }
+}
+</script>
---
+++ src/components/menus/MyGoodsType.vue
@@ -0,0 +1,131 @@
+<template>
+  <div class="block">
+    <el-table
+        :data="tableData"
+        style="width: 100%">
+      <el-table-column
+          prop="typeName"
+          label="类别名称"
+          width="180">
+      </el-table-column>
+      <el-table-column
+          prop="grade"
+          label="级别"
+          width="180">
+      </el-table-column>
+      <el-table-column
+          prop="firstId"
+          label="一级菜单id">
+      </el-table-column>
+      <el-table-column
+          prop="secondId"
+          label="二级菜单id">
+      </el-table-column>
+      <el-table-column label="操作">
+        <template slot-scope="scope">
+          <el-button
+              size="mini"
+              type="primary"
+              @click="handleEdit(scope.row)">编辑</el-button>
+          <el-button
+              size="mini"
+              type="primary"
+              @click="handleAddSub(scope.row)">新建子分类</el-button>
+          <el-button
+              size="mini"
+              type="danger"
+              @click="handleDelete(scope.row)">删除</el-button>
+        </template>
+      </el-table-column>
+    </el-table>
+    <el-pagination
+        @size-change="handleSizeChange"
+        @current-change="handleCurrentChange"
+        :current-page.sync="current"
+        :page-size="pageSize"
+        layout="prev, pager, next, jumper"
+        :total="total">
+    </el-pagination>
+  </div></template>
+
+<script>
+import {goodsTypeDeleteApi, goodsTypeListApi} from "../../utils/api";
+import da from "element-ui/src/locale/lang/da";
+
+export default {
+  name: 'MyGoodsType',
+  data(){
+    return{
+      // 用户列表数据
+      tableData: [],
+      current:0,
+      pageSize:5,
+      total:0,
+      //添加子分类时用的临时容器
+      temp:{
+        parentGrade:'',
+        parentId:''
+      }
+    }
+  },
+  methods:{
+    handleSizeChange(val) {
+      console.log(`每页 ${val} 条`);
+      goodsTypeListApi({'current':this.current,'pageSize':this.pageSize}).then(res=>{
+        console.log(res.data)
+        this.current=res.data.current
+        this.total=res.data.total
+        this.tableData=res.data.records
+      })
+    },
+    handleCurrentChange(val) {
+      console.log(`当前页: ${val}`);
+      goodsTypeListApi({'current':this.current,'pageSize':this.pageSize}).then(res=>{
+        console.log(res.data)
+        this.current=res.data.current
+        this.total=res.data.total
+        this.tableData=res.data.records
+      })
+    },
+    handleEdit(data){
+      console.log(data)
+      this.$router.push({
+        path:'/home/editGoodType',
+        query:{'goodId':data.id}
+      },)
+    },
+    handleDelete(data){
+      console.log(data)
+      goodsTypeDeleteApi({'id':data.id}).then(res=>{
+        this.$message({
+          message: '删除成功',
+          type: 'success'
+        });
+        this.$router.push('/home/checks')
+      })
+    },
+    handleAddSub(data){
+      console.log(data)
+      this.$router.push({
+        path:'/home/addGoodType',
+        query:{
+          'parentGrade':data.grade,
+          "parentId":data.id
+        }
+      })
+    }
+  },
+  created() {
+    goodsTypeListApi({'current':this.current,'pageSize':this.pageSize}).then(res=>{
+      console.log(res.data)
+      this.current=res.data.current
+      this.total=res.data.total
+      this.tableData=res.data.records
+    })
+  },
+
+}
+</script>
+
+<style lang="less" scoped>
+</style>
